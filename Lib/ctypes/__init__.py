0ee5a04ee21f842088e17d59087792a8327c2445
Dmitriy Kuminov <coding@dmik.org>
2015-12-10 21:28:23 +0000

    python: ctypes: Provide dummy _dlopen implementation.
    This makes mozilla's virtualenv work again in Firefox 31 and above.
    See #140 for details.
    
    
    git-svn-id: http://svn.netlabs.org/repos/rpm/python/trunk@602 978522c7-42b7-df11-88a9-00e081727c95

--- Lib/ctypes/__init__.py
+++ Lib/ctypes/__init__.py
@@ -140,6 +140,11 @@ if _os.name in ("nt", "ce"):
 elif _os.name == "posix":
     from _ctypes import dlopen as _dlopen
 
+elif _os.name == "os2":
+    # provide a dummy implmementation that always returns nothing (see #140)
+    def _dlopen(name, mode):
+        return
+
 from _ctypes import sizeof, byref, addressof, alignment, resize
 from _ctypes import get_errno, set_errno
 from _ctypes import _SimpleCData
