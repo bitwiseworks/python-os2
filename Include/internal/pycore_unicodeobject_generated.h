16d7d8047833e8b065b42428f077223106c4f04c
SilvanScherrer <silvan.scherrer@aroa.ch>
2025-04-10 16:56:15 +0200

    add Py_ID(os2) by hand, as regen is not running. reason is they use python code introduced in 3.10

--- Include/internal/pycore_unicodeobject_generated.h
+++ Include/internal/pycore_unicodeobject_generated.h
@@ -2236,6 +2236,10 @@ _PyUnicode_InitStaticStrings(PyInterpreterState *interp) {
     _PyUnicode_InternStatic(interp, &string);
     assert(_PyUnicode_CheckConsistency(string, 1));
     assert(PyUnicode_GET_LENGTH(string) != 1);
+    string = &_Py_ID(os2);
+    _PyUnicode_InternStatic(interp, &string);
+    assert(_PyUnicode_CheckConsistency(string, 1));
+    assert(PyUnicode_GET_LENGTH(string) != 1);
     string = &_Py_ID(out_fd);
     _PyUnicode_InternStatic(interp, &string);
     assert(_PyUnicode_CheckConsistency(string, 1));
